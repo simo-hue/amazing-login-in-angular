d72741865148183289bc6bff4dd16df0f8b7b986
simo-hue <mattioli.simone.10@gmail.com>
2021-05-20 17:49:29 +0200

    creazione mio login component e css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MioLoginComponent } from './mio-login/mio-login.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, MioLoginComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/mio-login/mio-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mio-login',
+  templateUrl: './mio-login.component.html',
+  styleUrls: ['./mio-login.component.css']
+})
+export class MioLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
