ec2b5b39805c9780bf4e4f9e86d5f207a84eb307
simo-hue <mattioli.simone.10@gmail.com>
2021-05-20 17:51:27 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MioLoginComponent } from './mio-login/mio-login.component';
+import { RouterModule } from '@angular/router';
+
+const LISTA_ROUTER = [
+  {path:''  ,component:MioLoginComponent},
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(LISTA_ROUTER) ],
   declarations: [ AppComponent, MioLoginComponent],
   bootstrap:    [ AppComponent ]
 })
