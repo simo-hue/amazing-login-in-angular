c41a4cf337f0f42cddf027e7d2a52da7e1d47f36
simo-hue <mattioli.simone.10@gmail.com>
2021-05-20 17:56:35 +0200

    1 file modified

--- src/app/mio-login/mio-login.component.ts
+++ src/app/mio-login/mio-login.component.ts
@@ -19,6 +19,33 @@ export class MioLoginComponent implements OnInit {
     password: ""
   };
 
-  
+ constructor(private router: Router, private loginService:LoginService, private http: HttpClient) { }
+
+  ngOnInit() {   
+    this.loginFail = false; 
+  }
+
+  encryptData() {
+    /* Crypto Username e Password */
+    this.http.get<any>('https://r89h4896t4h189q465sdjys.000webhostapp.com/api/?md5&username=' + this.username + '&password=' + this.password).subscribe(data => {
+      this.username = data.username;
+      this.password = data.password;
+
+      this.getRemoteDataBase();
+    })
+  }
+
+  getRemoteDataBase() {
+    this.http.get<any>('https://r89h4896t4h189q465sdjys.000webhostapp.com/api/?users').subscribe( data => { 
+      /* Se l'username esiste mi procuro la password associata */
+      for (let i = 0; i < data.length; i++) {   
+        if(data[i].hasOwnProperty(this.username)) {
+          this.utentiJson.password = data[i][this.username]["password"];
+        }
+      }
+
+      this.autenticazione();
+    }) 
+  }  
 
 }
